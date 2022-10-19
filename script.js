f8753b8ff70a735fd9b9c007cbf78bd3f51db166
Fares Qaddoura <frqd92@gmail.com>
2022-10-19 18:02:31 +0100

    add a green and red check to check validity on input change

--- script.js
+++ script.js
@@ -1,178 +1,70 @@
 
-
-
-//let inputBox = document.querySelectorAll(".input-style");
 let errorMessage = document.querySelectorAll(".error-message");
-let checkInvalid = document.querySelector(".check-invalid");
-let checkValid = document.querySelector(".check-valid");
-let validity = false;
-
-
-    document.addEventListener("focus",(e)=>{
-        let className = e.target.className;
-        let inputBox = document.querySelector("."+ className);
-        if(className.includes("name")){
-            if(className.includes("first")){
-                names(inputBox, 0);
-            }
-            else{
-                names(inputBox, 1);
-            }
-        };
-    },true)
 
 
 
-    function names (inputBox, index){
-  
-        inputBox.addEventListener("keydown", inputLogic);
-        function inputLogic(e){
-            errorMessage[index].textContent=""
-            let key = e.key;
-            inputBox.readOnly=false;
-            let inputVal = inputBox.value;
-
-            if(inputVal==" "){ //if first character is a space then it will automatically delete it
-                inputVal = inputVal.slice(0,0);
-                inputBox.value = inputVal;
-            }
-            else if(key.match(/[^a-zA-Z\s\-]/)){
-            errorMessage[index].textContent="(only alphabetic characters)"
-            inputBox.readOnly=true;
-            setTimeout(()=>{
-                errorMessage[index].textContent=""
-            },2000)
-            }
-            console.log(inputVal);
-
+document.addEventListener("focus",(e)=>{
+    let idName = e.target.id;
+    let inputBox = document.getElementById(idName);
+    if(idName.includes("name")){
+        if(idName.includes("first")){
+            names(inputBox, 0);
         }
-    
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// window.onload= function checkValidity(){    //this is where you left off. It's sending both 0 and 1 because window loads both. You need to set an if statement that only passes that parameter if that specific input is focussed
-//     inputBox[0].addEventListener("focus",inputString(0))
-//     inputBox[1].addEventListener("focus",inputString(1))
-
-// }
-
-
-// function inputString (){
-
-//     document.addEventListener("keydown", inputLogic)
-//     function inputLogic(e){
-    
-//         errorMessage[index].textContent=""
-//         let key = e.key;
-//         inputBox[index].readOnly=false;
-//         let inputVal = inputBox[index].value;
-//         if(inputVal[index]==" "){ //if first character is a space then it will automatically delete it
-//             inputVal = inputVal.slice(0,0);
-//             inputBox[index].value = inputVal;
-//         }
-//         else if(key.match(/[^a-zA-Z\s\-]/)){
-
-//             errorMessage[index].textContent="(only alphabetic characters)"
-//             inputBox[index].readOnly=true;
-//         }
-//     }
-
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function textValidity(e){
-
-//     //inputVal = inputBox[index].value;
-
-//     // switch(inputVal){
-//     //     case "": 
-//     //     validity = false;
-//     //     break;
-        
-//     //     case /[\d\W]/.test(inputVal) && inputVal:
-//     //     validity = false;
-//     //     break;
-
-//     //     default:
-//     //     validity = true;
-//     //     break;
-//     // }
-
-//     inputString(e);
-
-// }
-
-// function addRedCheck(){
-//     let inputVal = inputBox.value;
-//     if(inputVal.length === 0){
-//         checkInvalid.classList.remove("check-invalid")
-//         checkInvalid.classList.add("check-invalid-on")
-//     }
-//     else{
-//         checkInvalid.classList.add("check-invalid")
-//         checkInvalid.classList.remove("check-invalid-on")
-//         console.log(checkInvalid.classList);
-//     }
-//     console.log(inputVal.length)
-// }
-// function removeRedCheck(){
-
-// }
-
-// inputBox.addEventListener("focus",addRedCheck);
-
-// inputBox.addEventListener("keydown", addRedCheck)
-
-
-
-
-
-
-
-
-
-
+        else{
+            names(inputBox, 1);
+        }
+    };
+}, true)
+
+
+
+function inputCheck(inputBox, index){
+    console.log(index);
+    let iconContainer = document.querySelectorAll(".icon-container");
+    inputBox.addEventListener("change", ()=>{
+        if(inputBox.checkValidity()===true){
+            iconContainer[index].innerHTML="";
+            let icon = document.createElement("i");
+            icon.classList.add("fa-solid", "fa-check", "check-valid");
+            console.log(iconContainer[index].children.length)
+            if(iconContainer[index].children.length<2){
+                iconContainer[index].appendChild(icon);
+            };
+        }
+        else{
+            iconContainer[index].innerHTML="";
+            let icon = document.createElement("i");
+            icon.classList.add("fa-solid", "fa-x", "check-invalid");
+            if(iconContainer[index].children.length<2){
+                iconContainer[index].appendChild(icon);
+            };
+        }
+    });
+
+
+}
+
+function names (inputBox, index){
+    inputCheck(inputBox, index);
+    inputBox.addEventListener("keydown", e=>{
+        errorMessage[index].textContent=""
+        let key = e.key;
+        inputBox.readOnly=false;
+        let inputVal = inputBox.value;
+
+        if(inputVal==" "){ //if first character is a space then it will automatically delete it
+            inputVal = inputVal.slice(0,0);
+            inputBox.value = inputVal;
+        }
+        else if(key.match(/[^a-zA-Z\s\-]/)){
+        errorMessage[index].textContent="(only alphabetic characters)"
+        inputBox.readOnly=true;
+        setTimeout(()=>{
+            errorMessage[index].textContent=""
+        },1300);
+        }
+    },true);
+}
 
 
 let thisHover = document.querySelector(".this-hover");
