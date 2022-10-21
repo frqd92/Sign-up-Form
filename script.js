9dae584a1121a6dee77fd81a3be3dbd01941da33
Fares Qaddoura <frqd92@gmail.com>
2022-10-21 15:00:50 +0100

    finalize all validation logic and checks

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+//bug: if both passwords match but you  highlight input box and delete it doesn't update validity background to not green
 let errorMessage = document.querySelectorAll(".error-message");
 let passBox = document.querySelector(".pass-box");
 let passwordStorage ="";
@@ -27,22 +28,65 @@ document.addEventListener("focus",(e)=>{
         passBox.style.display="block";
         if(idName.includes("confirm")){
             passwordConfirm(inputBox, 5);
+            inputBox.onpaste = e => e.preventDefault(); //prevents copy pasting into password fields
             inputBox.addEventListener("click",()=>{passBox.style.display="block";})
         }
         else{
-            passwordChecker(inputBox, 4)
+            passwordChecker(inputBox, 4);
+            inputBox.onpaste = e => e.preventDefault();
             inputBox.addEventListener("click",()=>{passBox.style.display="block";})
         }
     }
     else{ //removes password box when focussing outside of password inputs
         passBox.style.display="none";
     }
+
 },true)
+
+function passwordChecks(match){ //adds green checks to password boxes when all the requirements are met
+    let sum= 0;
+    for(let x=0; x<li.length-2;x++){
+        sum += li[x].classList.length;
+    }    
+    if(sum===10 && iconContainer[4].childNodes.length==1){
+        iconContainer[4].innerHTML="";
+        let icon = document.createElement("i");
+        icon.classList.add("fa-solid", "fa-check", "check-valid");
+        iconContainer[4].appendChild(icon);
+        }
+    if (sum<10){
+        iconContainer[4].innerHTML="";
+        let icon = document.createElement("i");
+        icon.classList.add("fa-solid", "fa-x", "check-invalid");
+        iconContainer[4].appendChild(icon);
+    }
+    if(match){
+        iconContainer[5].innerHTML="";
+        let icon = document.createElement("i");
+        icon.classList.add("fa-solid", "fa-check", "check-valid");
+        iconContainer[5].appendChild(icon);
+    }
+    else if(!match && confirmInput.value){
+        console.log("hello")
+        iconContainer[5].innerHTML="";
+        let icon = document.createElement("i");
+        icon.classList.add("fa-solid", "fa-x", "check-invalid");
+        iconContainer[5].appendChild(icon);
+    }
+
+
+    // if(sum===12 && iconContainer[5].childNodes.length==0){
+    //     let icon = document.createElement("i");
+    //     icon.classList.add("fa-solid", "fa-check", "check-valid");
+    //     iconContainer[4].appendChild(icon);
+    //     iconContainer[5].appendChild(icon);
+    // }
+}
 
 let li = document.querySelectorAll(".list");
 function passwordChecker(inputBox, index){//checks password validity as user is typing... Could probably have written this cleaner
     passwordHover(inputBox);
-    inputBox.addEventListener("keydown", (e)=>{ 
+    inputBox.addEventListener("keydown", (e)=>{
         let key = e.key;
         if(inputBox.value.length>5){ //length
             li[0].classList.add("li-valid");
@@ -63,20 +107,21 @@ function passwordChecker(inputBox, index){//checks password validity as user is 
         if(inputBox.value === confirmInput){
             li[5].classList.add("li-valid");
         }
-         if(key==="Backspace"){
+        if(key==="Backspace"){
             let inputVal = inputBox.value;
             inputVal=inputVal.slice(0,-1);
             checker(inputVal);
-         }
-         inputBox.addEventListener("input",()=>{ //checks if passwords match as user types from Password field
+        }
+        inputBox.addEventListener("input",()=>{ //checks if passwords match as user types from Password field
             passwordStorage=inputBox.value;
+            passwordChecks()
             if(inputBox.value === confirmInput.value && (confirmInput.value!=="" && inputBox.value !=="")){
                 li[5].classList.add("li-valid");
             }
             else{
                 li[5].classList.remove("li-valid");
             }
-         })
+        })
     })
     function checker(inputVal){ //so it updates validity if user uses backspace
         if(inputVal.length<7){ //length
@@ -97,30 +142,6 @@ function passwordChecker(inputBox, index){//checks password validity as user is 
     }
 }
 
-function passwordConfirm(){ //checks if passwords match as user types from Password Confirm field
-    confirmInput.addEventListener("input", (e)=>{
-        let confirmString = confirmInput.value;
-        if(confirmString===passwordStorage){
-            li[5].classList.add("li-valid");
-        }
-        else{
-            li[5].classList.remove("li-valid");
-        }
-    })
-}
-
-function passwordHover(inputBox){ //hover on this and reveal pass
-    let passHover = document.getElementById("pass-reveal");
-    passHover.addEventListener("mouseover", ()=>{
-        inputBox.type="text";
-        confirmInput.type="text";
-        passHover.addEventListener("mouseleave",()=>{
-            inputBox.type="password";
-            confirmInput.type="password";
-        })
-    })
-}
-
 
 function validations (inputBox, index){ //to check validity as user types
     inputCheck(inputBox, index); //updates green and red icon as user is typing
@@ -139,7 +160,7 @@ function validations (inputBox, index){ //to check validity as user types
             inputVal = inputVal.slice(0,0);
             inputBox.value = inputVal;
         }
-        inputBox.addEventListener
+       
         if(index<2){ //if first or last names are selected
             if(key.match(/[^a-zA-Z\s\-]/)){
                 errorMessage[index].textContent="(only alphabetic characters)"
@@ -161,9 +182,8 @@ function validations (inputBox, index){ //to check validity as user types
         }
     });
 }
-
+let iconContainer = document.querySelectorAll(".icon-container");
 function inputCheck(inputBox, index){ //red and green check icons
-    let iconContainer = document.querySelectorAll(".icon-container");
     inputBox.addEventListener("input", checker, true);
         function checker(){
             if(inputBox.checkValidity()===true){
@@ -183,6 +203,31 @@ function inputCheck(inputBox, index){ //red and green check icons
                 };
             }
         }
+}
+
+function passwordConfirm(){ //checks if passwords match as user types from Password Confirm field
+    confirmInput.addEventListener("input", (e)=>{
+        let confirmString = confirmInput.value;
+        if(confirmString===passwordStorage && confirmString.length>0){
+            li[5].classList.add("li-valid");
+            passwordChecks(true);
+        }
+        else{
+            li[5].classList.remove("li-valid");
+            passwordChecks(false);
+        }
+    })
+}
+function passwordHover(inputBox){ //hover on this and reveal pass
+    let passHover = document.getElementById("pass-reveal");
+    passHover.addEventListener("mouseover", ()=>{
+        inputBox.type="text";
+        confirmInput.type="text";
+        passHover.addEventListener("mouseleave",()=>{
+            inputBox.type="password";
+            confirmInput.type="password";
+        })
+    })
 }
 
 document.addEventListener("mousedown", (e)=>{ //removes password box when clicking outside of password inputs
